84f04da4ee61475ee2b2b675fb9ccb6b11c159b0
Apocryphus13 <Triple_A13@yahoo.com>
2020-01-25 14:16:23 -0500

    Added Project Files

---
+++ EnterpriseConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EnterpriseConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            char gender;
+            double salary;
+            bool isEmployed;
+
+            Console.WriteLine("Enter your Name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter your Age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your Gender: ");
+            gender = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter your Salary: ");
+            salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Are you employed (true or false)");
+            isEmployed = bool.Parse(Console.ReadLine());
+        }
+    }
+}
